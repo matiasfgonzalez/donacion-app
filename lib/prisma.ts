db52a648897c1a8b882619a104826fdd35c26cd9
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2026-03-12 20:18:59 -0300

    fix build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/index.js';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
 const globalForPrisma = globalThis as unknown as {
